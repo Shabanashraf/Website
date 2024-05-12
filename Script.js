b5721fbfe8be5931312a2e3f3d11a58c976d4d01
Shabana Ashraf <168170803+Shabanashraf@users.noreply.github.com>
2024-05-12 22:51:15 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  // Add your custom JavaScript here
+  $('#actionButton').click(function() {
+    // Action to perform when the button is clicked
+    alert('Flower of the Day: Rose!');
+  });
+});
